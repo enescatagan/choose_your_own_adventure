68e91295c8f7ac80efc13d1823c62a8662928266
Enes Atağan <enescatagan@gmail.com>
2025-11-22 14:19:58 +0300

    DataBase Setup

---
+++ backend/models/story.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from db.database import Base
+
+class Story():
+    pass
